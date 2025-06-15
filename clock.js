7173d836fb8d122ab510cf93c92aab9a94c36996
Virang Acharya <104147123+Virang007@users.noreply.github.com>
2025-06-16 00:43:23 +0530

    Add files via upload

---
+++ clock.js
@@ -0,0 +1,16 @@
+const e = React.createElement;
+
+function MyApp() {
+  return e("div", null, [
+    e("h2", { key: 1 }, "🚀 Hello from virang acharya!"),
+    e("p", { key: 2 }, "This React app runs without build or JSX."),
+    e("button", {
+      key: 3,
+      onClick: () => alert("🎉 Nices")
+    }, "Click Me")
+  ]);
+}
+
+const rootId = document.currentScript.previousElementSibling.id;
+const root = ReactDOM.createRoot(document.getElementById(rootId));
+root.render(e(MyApp));
